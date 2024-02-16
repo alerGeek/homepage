86e4029b03a7eec5eae6d34d756c3947a98f3569
alerGeek <promucha@gmail.com>
2024-02-16 16:57:04 +0100

    cleanup - remove not used dir and plugins

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  title: 'Portfolio Dev',
-  deployDate: '<INSERT_DATE>',
-  gitSha: '<INSERT_SHA>',
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  title: 'Portfolio',
-  deployDate: '2024-02-14',
-  gitSha: '<INSERT_SHA>',
-};
